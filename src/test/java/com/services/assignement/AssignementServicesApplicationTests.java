501247875aa1c90c3f78129620dcfca71a42610a
Frank_G <guilombofrank07@gmail.com>
2026-03-11 12:16:58 -0500

    git init

---
+++ src/test/java/com/services/assignement/AssignementServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.assignement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignementServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
